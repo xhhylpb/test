06646cd5bd38565a3ee9b18229985c9daa689b56
‘pb.li’ <18636924029@163.com>
2017-09-20 03:16:09 +0800

    修改缓存

--- test/src/main/java/com/xhhy/dao/UserDao.java
+++ test/src/main/java/com/xhhy/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.xhhy.dao;
 
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import com.xhhy.bean.UserBean;
